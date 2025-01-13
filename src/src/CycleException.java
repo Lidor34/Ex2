1f5ad11333c872bcbd94b25351daa58caf5e673f
Lidor34 <lidor0018@gmail.com>
2025-01-13 23:07:31 +0200

    Delete src/src directory

--- src/src/CycleException.java
+++
@@ -1,7 +0,0 @@
-// Custom exception to handle circular dependencies (cycle errors)
-public class CycleException extends Exception {
-    // Constructor that accepts a message
-    public CycleException(String message) {
-        super(message);  // Pass the message to the parent Exception class
-    }
-}
